86ca6c0b2c4c306efa2585e2c2c032896b5ef706
fierysleo <jolexandersan@gmail.com>
2025-09-19 00:06:23 +0800

    form mostly finished
    button work in progress

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+const first_name = document.querySelector("#firstname")
+const last_name = document.querySelector("#lastname")
+const email = document.querySelector("#user_email")
+const phone_number = document.querySelector("#number")
+const password = document.querySelector("#password")
+const confirm_password = document.querySelector("#confirmPassword")
+const sign_up_button = document.querySelector("#signUpButton")
+
+signUpButton.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    console.log(first_name.value);
+    console.log(last_name.value);
+    console.log(email.value);
+    console.log(phone_number.value);
+    })
