20754c1da094672e4afe3f2233b728516f620ccf
fierysleo <jolexandersan@gmail.com>
2025-09-19 00:45:31 +0800

    75% done added function in buttons

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const first_name = document.querySelector("#firstname")
-const last_name = document.querySelector("#lastname")
+const first_name = document.querySelector("#firstName")
+const last_name = document.querySelector("#lastName")
 const email = document.querySelector("#user_email")
-const phone_number = document.querySelector("#number")
+const number = document.querySelector("#number")
 const password = document.querySelector("#password")
 const confirm_password = document.querySelector("#confirmPassword")
-const sign_up_button = document.querySelector("#signUpButton")
 
-signUpButton.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    console.log(first_name.value);
-    console.log(last_name.value);
-    console.log(email.value);
-    console.log(phone_number.value);
-    })
+
+const submitButton = document.getElementById("signUpButton");
+
+submitButton.addEventListener('click', (event) => {
+   console.log(first_name.value)
+   console.log(last_name.value)
+   console.log(email.value)
+   console.log(number.value)
+   console.log(password.value)
+   console.log(confirm_password.value)
+})
